49bdd46dc4955bb8d64d8a87f72dc74ee3a16fab
solvld <sol.vld@yandex.ru>
2025-07-09 20:30:45 +0200

    update readme

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { InputForm } from '@/components/Form'
 export default function Home() {
   return (
     <main className="flex min-h-full p-4 lg:px-32 flex-col items-center justify-between">
-      <Wallet />
       <InputForm />
     </main>
   )
